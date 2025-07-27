1afd97945cf691fd3b5aabdd1377c36b32e649a0
BaranIga <baran.iga494@gmail.com>
2025-07-27 11:11:00 +0200

    Aktualizacja plików

--- 03-drag-and-drop/script.js
+++ 03-drag-and-drop/script.js
@@ -1,9 +1,9 @@
 const cards = document.querySelectorAll(".card");
 const lists = document.querySelectorAll(".list");
 
-for(const card of cards) {
-  card.addEventListener("dargstart", dragStart)
-  card.addEventListener("dragend", dragEnd)
+for (const card of cards) {
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
 }
 
 for (const list of lists) {
@@ -13,7 +13,7 @@ for (const list of lists) {
   list.addEventListener("drop", dragDrop);
 }
 
-function dargstart(e) {
+function dragStart(e) {
   e.dataTransfer.setData("text/plain", this.id);
 }
 
@@ -21,3 +21,23 @@ function dragEnd() {
   console.log("Drag ended");
 }
 
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  this.classList.remove("over");
+}
+
+function dragDrop(e) {
+  const id = e.dataTransfer.getData("text/plain");
+  const card = document.getElementById(id);
+
+  this.appendChild(card);
+  this.classList.remove("over");
+}
